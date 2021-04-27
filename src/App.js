aa7e394fcb4cea8c9f2200658c8a6cb3ba8d5b2f
clippefuentes <clippefuentes@gmail.com>
2021-04-26 21:42:31 -0600

    do multiple context on 1 component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from 'react';
 import Navbar from './components/Navbar'
 import ToDoList from './components/ToDoList'
 import ThemeContextProvider from './contexts/ThemeContext'
+import AuthContextProvider from './contexts/AuthContext'
 
 function App() {
   return (
     <div className="App">
       <div className="ui raised very padded text container segment">
-        <ThemeContextProvider>
-          <Navbar />
-          <ToDoList />
-        </ThemeContextProvider>
+        <AuthContextProvider>
+          <ThemeContextProvider>
+            <Navbar />
+            <ToDoList />
+          </ThemeContextProvider>
+        </AuthContextProvider>
       </div>
     </div>
   );
